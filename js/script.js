286954b33e28d387bd762dfe46a0e8b6078a6521
Mackewinsson <Mackewinsson@gmail.com>
2019-12-12 19:33:41 -0300

    seach bar and functionality in place

--- js/script.js
+++ js/script.js
@@ -71,17 +71,70 @@ const appendPageLinks = (list) => {
       showPage(studentList, event.target.textContent);
    });
 };
+
+
+const searchBar = (list) => {
+// Select the .page-header element
+const pageHeader = document.querySelector('div.page-header');
+// Create the div element and add the student-search class
+const divStudentSearch = document.createElement('div');
+divStudentSearch.className = 'student-search';
+// Create input and adding placeholder
+const input = document.createElement('input');
+input.placeholder = 'Search for students...';
+// Create button and adding textContent Search
+const button = document.createElement('button');
+button.textContent = 'Search';
+// Appending the elements to the dom
+divStudentSearch.appendChild(button);
+divStudentSearch.appendChild(input);
+pageHeader.appendChild(divStudentSearch);
+
+// Add event listener
+
+button.addEventListener('click', () =>{
+
+      const h3 = document.getElementsByTagName('h3');
+
+      for(let i = 0; i < h3.length; i += 1) {
+            let title = h3[i].textContent;      
+            if(title.toLowerCase().indexOf(input.value) != -1){
+
+                  list[i].style.display = 'block';
+
+            }else{
+                  
+                  list[i].style.display = 'none';
+            };
+      }
+});
+
+
+input.addEventListener('keyup', () =>{
+
+      const h3 = document.getElementsByTagName('h3');
+
+      for(let i = 0; i < h3.length; i += 1) {
+            let title = h3[i].textContent;      
+            if(title.toLowerCase().indexOf(input.value) != -1){
+
+                  list[i].style.display = 'block';
+
+            }else{
+                  
+                  list[i].style.display = 'none';
+            };
+      }
+});
+
+
+};
+
+// calls appendPageLinks()
+
+searchBar(studentList);
+
 // calls appendPageLinks() to dinamicaly add pagination buttons
 appendPageLinks(studentList);
 // calls the showPage() to show the right elements on the pagination
 showPage(studentList, 1);
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
-***/
-
-
-
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
